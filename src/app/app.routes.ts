d20a5f4635884d352f1cc55ffd0607e242fb99ad
Vadim-Maul <v.v.maul681@gmail.com>
2025-11-24 20:44:17 +0100

    bootstrap angular app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainlayoutComponent } from '@layout/mainlayout/mainlayout.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainlayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+    ],
+  },
+];
